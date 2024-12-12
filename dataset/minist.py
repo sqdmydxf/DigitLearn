e2cfb44a2152d83eea23687b822bfa1e7ccfae95
sqdmydxf <sqdmydxf@gmail.com>
2024-12-12 23:28:14 +0800

    初始化项目

--- dataset/minist.py
+++ dataset/minist.py
@@ -3,15 +3,50 @@ import struct
 from pathlib import Path
 
 from icecream import ic
+import numpy as np
+
 
 current_dir = Path(__file__).parent
 
-def load_data():
-    with gzip.open(current_dir / 'train-images-idx3-ubyte.gz', 'rb') as f:
+def read_images(path):
+    with gzip.open(path, 'rb') as f:
         magic, num_images = struct.unpack('>II', f.read(8))
-        ic(magic, num_images)
         num_rows, num_cols = struct.unpack('>II', f.read(8))
-        ic(num_rows, num_cols)
+        num_pixels = num_rows * num_cols
+        data = np.frombuffer(f.read(), dtype='uint8').reshape(num_images, num_pixels, 1)
+        ic(f"{path} : {data.shape}")
+    return data
 
-load_data()
+def read_labels(path):
+    with gzip.open(path, 'rb') as f:
+        f.read(8)
+        data = np.frombuffer(f.read(), dtype='uint8')
+        ic(f"{path} : {data.shape}")
+    return data
+
+def load_data():
+    train_inputs = read_images(current_dir / 'train-images-idx3-ubyte.gz')
+    train_outputs = read_labels(current_dir / 'train-labels-idx1-ubyte.gz')
+    test_inputs = read_images(current_dir / 't10k-images-idx3-ubyte.gz')
+    test_outputs = read_labels(current_dir / 't10k-labels-idx1-ubyte.gz')
+    return train_inputs, train_outputs, test_inputs, test_outputs
+
+
+def load_data_wrapper():
+    train_inputs, train_outputs, test_inputs, test_outputs = load_data()
+    train_data = zip(train_inputs, [convert_vector(output) for output in train_outputs])
+    ic(next(train_data)[0].shape)
+    ic(next(train_data)[1].shape)
+    test_data = zip(test_inputs, test_outputs)
+    ic(next(test_data)[0].shape)
+    ic(next(test_data)[1].shape)
+    return train_data, test_data
+
+def convert_vector(output):
+    vector = np.zeros((10, 1))
+    vector[output] = 1.0
+    return vector
+
+
+load_data_wrapper()
 
